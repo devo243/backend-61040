7d3325bed39e4638e9ad6facec25e56c552d241d
devo243 <cbrian@mit.edu>
2024-10-03 21:04:56 -0400

    Added the communiting and favoriting concepts

---
+++ server/concepts/communiting.ts
@@ -0,0 +1,234 @@
+import { ObjectId } from "mongodb";
+
+import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotAllowedError, NotFoundError } from "./errors";
+
+export interface CommunityDoc extends BaseDoc {
+  author: ObjectId;
+  title: string;
+  description: string;
+  items: ObjectId[];
+  members: ObjectId[];
+}
+
+export default class CommunitingConcept {
+  public readonly communities: DocCollection<CommunityDoc>;
+
+  constructor(collectionName: string) {
+    this.communities = new DocCollection<CommunityDoc>(collectionName);
+  }
+
+  async create(author: ObjectId, title: string, description: string) {
+    const newItemsArray = <ObjectId[]>[];
+    const newMembersArray = <ObjectId[]>[author];
+    const _id = await this.communities.createOne({ author, title, description, items: newItemsArray, members: newMembersArray });
+
+    return { msg: "Community Succesfully Created!", community: await this.communities.readOne({ _id }) };
+  }
+
+  async delete(_id: ObjectId) {
+    await this.communities.deleteOne({ _id });
+    return { msg: "Community Succesfully Deleted!" };
+  }
+
+  async getCommunityByID(_id: ObjectId) {
+    return await this.communities.readOne({ _id });
+  }
+
+  async getCommunities(_id: ObjectId) {
+    return await this.communities.readMany({}, { sort: { _id: -1 } });
+  }
+
+  async getCommunityByTitle(title: string) {
+    return await this.communities.readOne({ title });
+  }
+
+  async getNumMembers(_id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    const members = community.members;
+
+    return { numMembers: members.length };
+  }
+
+  async join(user: ObjectId, _id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    await this.assertUserNotInCommunity(user, _id);
+
+    const newMembersArray = community?.members;
+    newMembersArray?.push(user);
+
+    await this.communities.partialUpdateOne({ _id }, { members: newMembersArray });
+    return { msg: "A user has joined the community!" };
+  }
+
+  async leave(user: ObjectId, _id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    await this.assertUserInCommunity(user, _id);
+
+    const oldMembersArray = community?.members;
+    const newMembersArray = oldMembersArray?.filter((e) => e.toString() !== user.toString());
+
+    await this.communities.partialUpdateOne({ _id }, { members: newMembersArray });
+    return { msg: "A user has left the community!" };
+  }
+
+  async addItem(item: ObjectId, _id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    await this.assertItemNotInCommunity(item, _id);
+
+    const newItemsArray = community?.items;
+    newItemsArray?.push(item);
+
+    await this.communities.partialUpdateOne({ _id }, { items: newItemsArray });
+    return { msg: "An item has been added to the community!" };
+  }
+
+  async deleteItem(item: ObjectId, _id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    await this.assertItemInCommunity(item, _id);
+
+    const oldItemsArray = community?.items;
+    const newItemsArray = oldItemsArray?.filter((e) => e.toString() !== item.toString());
+
+    await this.communities.partialUpdateOne({ _id }, { items: newItemsArray });
+    return { msg: "An item has been deleted from the community!" };
+  }
+
+  async getItems(_id: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    return community?.items;
+  }
+
+  checkObjectIdInArray(_id: ObjectId, arr: ObjectId[]) {
+    for (const id of arr) {
+      if (_id.equals(id)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  async assertAuthorIsUser(_id: ObjectId, user: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    if (community.author.toString() !== user.toString()) {
+      throw new CommunityAuthorNoMatchError(user, _id);
+    }
+  }
+
+  async assertUserInCommunity(_id: ObjectId, user: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    const members = community.members;
+
+    if (!this.checkObjectIdInArray(user, members)) {
+      throw new CommunityUserNoMatchError(user, _id);
+    }
+  }
+
+  async assertUserNotInCommunity(_id: ObjectId, user: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    const members = community.members;
+
+    if (this.checkObjectIdInArray(user, members)) {
+      throw new CommunityUserNoMatchError(user, _id);
+    }
+  }
+
+  async assertItemInCommunity(_id: ObjectId, item: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    const items = community.items;
+
+    if (!this.checkObjectIdInArray(item, items)) {
+      throw new CommunityUserNoMatchError(item, _id);
+    }
+  }
+
+  async assertItemNotInCommunity(_id: ObjectId, item: ObjectId) {
+    const community = await this.getCommunityByID(_id);
+
+    if (!community) {
+      throw new NotFoundError(`Community ${_id} doesn't exist!`);
+    }
+
+    const items = community.items;
+
+    if (this.checkObjectIdInArray(item, items)) {
+      throw new CommunityUserNoMatchError(item, _id);
+    }
+  }
+}
+
+export class CommunityAuthorNoMatchError extends NotAllowedError {
+  constructor(
+    public readonly author: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is not the author of community {1}", author, _id);
+  }
+}
+
+export class CommunityUserNoMatchError extends NotFoundError {
+  constructor(
+    public readonly member: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is not a member of community {1}", member, _id);
+  }
+}
+
+export class CommunityMemberExistsError extends NotAllowedError {
+  constructor(
+    public readonly member: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is already a member of community {1}", member, _id);
+  }
+}
+
+export class CommunityItemNoMatchError extends NotFoundError {
+  constructor(
+    public readonly item: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is not an item of community {1}", item, _id);
+  }
+}
+
+export class CommunityItemExistsError extends NotAllowedError {
+  constructor(
+    public readonly item: ObjectId,
+    public readonly _id: ObjectId,
+  ) {
+    super("{0} is already an item of community {1}", item, _id);
+  }
+}
---
+++ server/concepts/favoriting.ts
@@ -0,0 +1,77 @@
+import { ObjectId } from "mongodb";
+
+import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotAllowedError, NotFoundError } from "./errors";
+
+export interface FavoriteDoc extends BaseDoc {
+  user: ObjectId;
+  item: ObjectId;
+}
+
+export default class FavoritingConcept {
+  public readonly favorites: DocCollection<FavoriteDoc>;
+
+  constructor(collectionName: string) {
+    this.favorites = new DocCollection<FavoriteDoc>(collectionName);
+  }
+
+  async favorite(user: ObjectId, item: ObjectId) {
+    this.assertItemIsNotFavorite(user, item);
+
+    const _id = await this.favorites.createOne({ user, item });
+
+    return { msg: "A user has favorited an item!", favorite: await this.favorites.readOne({ _id }) };
+  }
+
+  async unfavorite(user: ObjectId, item: ObjectId) {
+    this.assertItemIsFavorite(user, item);
+
+    await this.favorites.deleteOne({ user: user, item: item });
+
+    return { msg: "A user has unfavorited an item!" };
+  }
+
+  async getNumFavorites(item: ObjectId) {
+    const favs = await this.favorites.readMany({ item: item });
+
+    if (favs === null) {
+      return { numFavorites: 0 };
+    } else {
+      return { numFavorites: favs.length };
+    }
+  }
+
+  async assertItemIsFavorite(user: ObjectId, item: ObjectId) {
+    const favorite = await this.favorites.readOne({ user: user, item: item });
+
+    if (!favorite) {
+      throw new FavoriteItemNoMatchError(user, item);
+    }
+  }
+
+  async assertItemIsNotFavorite(user: ObjectId, item: ObjectId) {
+    const favorite = await this.favorites.readOne({ user: user, item: item });
+
+    if (favorite) {
+      throw new FavoriteItemExistsError(user, item);
+    }
+  }
+}
+
+export class FavoriteItemNoMatchError extends NotFoundError {
+  constructor(
+    public readonly user: ObjectId,
+    public readonly item: ObjectId,
+  ) {
+    super("{0} has not favorited item {1}", user, item);
+  }
+}
+
+export class FavoriteItemExistsError extends NotAllowedError {
+  constructor(
+    public readonly user: ObjectId,
+    public readonly item: ObjectId,
+  ) {
+    super("{0} has already favorited item {1}", user, item);
+  }
+}
